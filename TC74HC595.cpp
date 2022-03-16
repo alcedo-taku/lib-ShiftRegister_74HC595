61764bd1ded31e00d513e0b093ca45c1a0afaccb
立脇拓 <takuji.coroton.thanks@gmail.com>
2022-03-16 12:40:13 +0900

    first commit

---
+++ TC74HC595.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file TC74HC595.cpp
+ * @brief
+ * @date Mar 16, 2022
+ */
+
+#include <TC74HC595.hpp>
+
+namespace tc74hc595 {
+
+TC74HC595::TC74HC595() {
+}
+
+} /* namespace tc74hc595 */
---
+++ TC74HC595.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file TC74HC595.hpp
+ * @brief
+ * @date Mar 16, 2022
+ */
+
+#ifndef TC74HC595_HPP_
+#define TC74HC595_HPP_
+
+namespace tc74hc595 {
+
+class TC74HC595 {
+public:
+	TC74HC595();
+};
+
+} /* namespace tc74hc595 */
+
+#endif /* TC74HC595_HPP_ */
